3c7d54fe1b851e4ec00a285b7bebfdf40fc8c6b1
svetoslav.hristov <svetoslav.hristov2001@gmail.com>
2023-12-07 10:38:41 +0100

    initial commit

---
+++ src/main/java/com/fightfoodwaste/useraccountservice/UserAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fightfoodwaste.useraccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAccountServiceApplication.class, args);
+    }
+
+}
