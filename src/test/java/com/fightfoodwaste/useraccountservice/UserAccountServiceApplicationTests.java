57aa90d323836bef706400a6a338a3ea776a3243
svetoslav.hristov <svetoslav.hristov2001@gmail.com>
2024-01-16 22:37:12 +0100

    springboottest fixed

--- src/test/java/com/fightfoodwaste/useraccountservice/UserAccountServiceApplicationTests.java
+++ src/test/java/com/fightfoodwaste/useraccountservice/UserAccountServiceApplicationTests.java
@@ -1,9 +1,6 @@
 package com.fightfoodwaste.useraccountservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UserAccountServiceApplicationTests {
 
 
